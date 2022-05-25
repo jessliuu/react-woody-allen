4c27c22c5e5b60bdcb6e3de82e9d655288c77d8a
tszkiu <tszkiu@gmail.com>
2022-05-25 16:45:20 +0200

    global context

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import LogIn from "./Views/LogIn.js";
 import NoMatch from "./Views/NoMatch.js";
 import More from "./Views/More.js";
 import Register from "./Views/Register.js";
+import { MovieContextProvider } from "./Contexts/MovieContext";
 
 // import Movies from "./Components/Movies";
 import Navigation from "./Components/Navbar/Navbar.js";
+import React from "react";
 
 function App() {
   // <Browse />;
@@ -17,15 +19,17 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/discuss" element={<Discuss />} />
-        <Route path="/browse" element={<Browse />} />
-        <Route path="/browse/:title" element={<More />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <MovieContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/discuss" element={<Discuss />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/browse/:title" element={<More />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </MovieContextProvider>
     </div>
   );
 }
--- src/Views/Browse.js
+++ src/Views/Browse.js
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import Movie from "../Components/Movie";
 import { Row, Button } from "react-bootstrap";
+import { MovieContext } from "../Contexts/MovieContext";
 
 const Browse = () => {
-  const [movies, setMovies] = useState();
-
-  const urls = [
-    "https://api.themoviedb.org/3/movie/8217?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/19200?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/28384?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/10440?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/11382?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/10462?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/11448?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/9716?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/2639?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/9466?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/9684?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/10569?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/2779?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/48208?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-    "https://api.themoviedb.org/3/movie/9689?api_key=a8ba99c690b00811c8c588b3b76b46f4",
-  ];
-  const fetchMovies = async () => {
-    try {
-      const responses = await Promise.all(urls.map((url) => fetch(url)));
-      const data = await Promise.all(responses.map((r) => r.json()));
-      setMovies(data);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { movies, fetchMovies } = useContext(MovieContext);
 
   useEffect(() => {
     fetchMovies();
